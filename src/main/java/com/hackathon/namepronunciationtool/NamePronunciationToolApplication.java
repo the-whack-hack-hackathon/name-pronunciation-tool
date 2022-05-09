eba44214f1ddc3795044829e6e7b728904cf4033
srikanth <srikanth.bobbasani@gmail.com>
2022-05-09 20:53:57 +0530

    initial commit

---
+++ src/main/java/com/hackathon/namepronunciationtool/NamePronunciationToolApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.namepronunciationtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamePronunciationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamePronunciationToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/namepronunciationtool/NamePronunciationToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.namepronunciationtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamePronunciationToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
